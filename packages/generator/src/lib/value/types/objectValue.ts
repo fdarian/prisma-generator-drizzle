ae38ca32b7ce08175bfed9f8a5491ddc1cb88aa3
farreldarian <farreldarian@users.noreply.github.com>
2023-12-16 18:52:08 +0700

    Fix when empty entries

--- packages/generator/src/lib/value/types/objectValue.ts
+++ packages/generator/src/lib/value/types/objectValue.ts
@@ -5,6 +5,7 @@ export type Entry = [string, IValue]
 export function objectValue(entries: Entry[]) {
   return createValue({
     render(): string {
+      if (entries.length === 0) return `{}`
       return `{ ${entries
         .map(([key, value]) => `${key}: ${value.render()}`)
         .join(', ')} }`
