b784a1fe2708e24bc29bb60bbb7add940810c67e
farreldarian <farreldarian@users.noreply.github.com>
2023-12-17 11:36:00 +0700

    Update usage

--- packages/usage/src/index.ts
+++ packages/usage/src/index.ts
@@ -1,11 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { Client } from 'pg'
+import { schema } from 'prisma/drizzle/schema'
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
 })
 
 await client.connect()
-const db = drizzle(client)
+const db = drizzle(client, { schema })
 
 process.exit(0)
