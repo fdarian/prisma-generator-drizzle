e24dadcefe5f41e0100e5ee85898a9a39660f554
farreldarian <farreldarian@users.noreply.github.com>
2023-12-21 23:19:04 +0700

    Allow passing object

--- packages/generator/src/lib/value/types/objectValue.ts
+++ packages/generator/src/lib/value/types/objectValue.ts
@@ -3,11 +3,15 @@ import { UseVarValue } from './useVarValue'
 
 export type Entry = [string, IValue]
 
-export function objectValue(entries: (Entry | UseVarValue)[]) {
+export function objectValue(
+  entries: Record<string, IValue> | (Entry | UseVarValue)[]
+) {
+  const _entries = Array.isArray(entries) ? entries : Object.entries(entries)
+
   return createValue({
     render(): string {
       if (entries.length === 0) return `{}`
-      return `{ ${entries
+      return `{ ${_entries
         .map((entryOrVar) =>
           Array.isArray(entryOrVar)
             ? `${entryOrVar[0]}: ${entryOrVar[1].render()}`
