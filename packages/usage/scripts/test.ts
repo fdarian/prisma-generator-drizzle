13f01b50b7690790c3358c0b1e8ea274ae07851c
farreldarian <farreldarian@users.noreply.github.com>
2023-12-25 00:42:24 +0700

    Revert "Try pnpm-node equivalent"
    This reverts commit 3aa8864d6164216dabd0daf4fbdc63fa4c4a91f8.

--- packages/usage/scripts/test.ts
+++ packages/usage/scripts/test.ts
@@ -1,6 +1,5 @@
 import { execSync } from 'child_process'
 import { object, parse, string, url } from 'valibot'
-import 'dotenv/config'
 
 const env = parse(
   object({
@@ -12,15 +11,15 @@ const env = parse(
 
 const promises = [
   execSync(
-    `DATABASE_URL=${env.PG_DATABASE_URL} prisma db push --schema prisma/schema.prisma --force-reset --accept-data-loss`
+    `DATABASE_URL=${env.PG_DATABASE_URL} bun prisma db push --schema prisma/schema.prisma --force-reset --accept-data-loss`
   ),
   execSync(
-    `DATABASE_URL=${env.MYSQL_DATABASE_URL} prisma db push --schema prisma/mysql/schema.prisma --force-reset --accept-data-loss`
+    `DATABASE_URL=${env.MYSQL_DATABASE_URL} bun prisma db push --schema prisma/mysql/schema.prisma --force-reset --accept-data-loss`
   ),
 ]
 
 await Promise.all(promises)
 
-execSync('vitest run --globals', { stdio: 'inherit' })
+execSync('bun test', { stdio: 'inherit' })
 
 process.exit(0)
