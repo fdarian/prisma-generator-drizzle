a72ef7cc569699cd2f7787218c4209ec160abfd2
farreldarian <farreldarian@users.noreply.github.com>
2023-12-26 21:38:32 +0700

    Create test for disambiguating relations

--- packages/usage/tests/dummy.ts
+++ packages/usage/tests/dummy.ts
@@ -1,4 +1,6 @@
+import { createId } from '@paralleldrive/cuid2'
 import { teams } from 'prisma/drizzle/teams'
+import { transfers } from 'prisma/drizzle/transfers'
 import { users } from 'prisma/drizzle/users'
 
 export const user_insert = {
@@ -38,7 +40,35 @@ export const user2_result: typeof users.$inferSelect = {
   teamId: null,
 }
 
+export const user3_insert = {
+  id: 3,
+  name: 'Bob',
+  email: 'bob@email.com',
+  enum: 'TypeTwo',
+} satisfies typeof users.$inferInsert
+
 export const team: typeof teams.$inferInsert = {
   id: 1,
   name: 'Team',
+}
+
+export const transfer_user1_to_user2_insert: typeof transfers.$inferInsert = {
+  id: createId(),
+  fromId: user_insert.id,
+  toId: user2_insert.id,
+  createdAt: new Date(),
+}
+
+export const transfer_user2_to_user1_insert: typeof transfers.$inferInsert = {
+  id: createId(),
+  fromId: user2_insert.id,
+  toId: user_insert.id,
+  createdAt: new Date(),
+}
+
+export const transfer_user1_to_user3_insert: typeof transfers.$inferInsert = {
+  id: createId(),
+  fromId: user_insert.id,
+  toId: user3_insert.id,
+  createdAt: new Date(),
 }
--- packages/usage/tests/postgres.test.ts
+++ packages/usage/tests/postgres.test.ts
@@ -1,16 +1,23 @@
 import { teams } from 'prisma/drizzle/teams'
+import { transfers } from 'prisma/drizzle/transfers'
 import { users } from 'prisma/drizzle/users'
 import { db } from 'src/lib/postgres'
 import {
   team,
+  transfer_user1_to_user2_insert,
+  transfer_user1_to_user3_insert,
+  transfer_user2_to_user1_insert,
   user2_insert,
   user2_result,
+  user3_insert,
   user_insert,
   user_result,
 } from './dummy'
 
 beforeEach(async () => {
+  await db.delete(transfers)
   await db.delete(users)
+  await db.delete(teams)
 })
 
 test('insert + select', async () => {
@@ -63,4 +70,58 @@ test('relations', async () => {
     teamId: team.id,
     team: team,
   })
+})
+
+test('disambiguating relations', async () => {
+  await db.insert(users).values(user_insert)
+  await db.insert(users).values(user2_insert)
+  await db.insert(users).values(user3_insert)
+  await db.insert(transfers).values(transfer_user1_to_user2_insert)
+  await db.insert(transfers).values(transfer_user2_to_user1_insert)
+  await db.insert(transfers).values(transfer_user1_to_user3_insert)
+
+  const transfer = await db.query.transfers.findFirst({
+    where: (Transfer, { eq }) =>
+      eq(Transfer.id, transfer_user1_to_user2_insert.id),
+    with: { from: { columns: { id: true } }, to: { columns: { id: true } } },
+  })
+  if (!transfer) throw new Error('transfer is null')
+  expect(transfer.from).toStrictEqual({ id: user_result.id })
+  expect(transfer.to).toStrictEqual({ id: user2_result.id })
+
+  async function fetchUser(userId: number) {
+    const user = await db.query.users.findFirst({
+      where: (User, { eq }) => eq(User.id, userId),
+      with: {
+        receivedTransfers: true,
+        sentTransfers: true,
+      },
+    })
+    if (!user) throw new Error('user is null')
+    return user
+  }
+
+  const user1 = await fetchUser(user_insert.id)
+
+  expect(user1.sentTransfers).toStrictEqual([
+    transfer_user1_to_user2_insert,
+    transfer_user1_to_user3_insert,
+  ])
+  expect(user1.receivedTransfers).toStrictEqual([
+    transfer_user2_to_user1_insert,
+  ])
+
+  const user2 = await fetchUser(user2_insert.id)
+
+  expect(user2.sentTransfers).toStrictEqual([transfer_user2_to_user1_insert])
+  expect(user2.receivedTransfers).toStrictEqual([
+    transfer_user1_to_user2_insert,
+  ])
+
+  const user3 = await fetchUser(user3_insert.id)
+
+  expect(user3.sentTransfers).toStrictEqual([])
+  expect(user3.receivedTransfers).toStrictEqual([
+    transfer_user1_to_user3_insert,
+  ])
 })
