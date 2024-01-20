1cd07011f8c536973d736399e0afa49ed43c89e6
farreldarian <farreldarian@users.noreply.github.com>
2024-01-20 19:40:16 +0700

    use throwIfNull

--- packages/usage/tests/shared/testDefault.ts
+++ packages/usage/tests/shared/testDefault.ts
@@ -2,6 +2,7 @@ import { createId, isCuid } from '@paralleldrive/cuid2'
 import { isAfter } from 'date-fns'
 import Decimal from 'decimal.js'
 import { eq, inArray } from 'drizzle-orm'
+import { throwIfnull } from 'tests/utils/query'
 import { TestContext } from 'tests/utils/types'
 import { setTimeout } from 'timers/promises'
 import { describe, expect, test } from 'vitest'
@@ -16,38 +17,40 @@ export function testDefault({ db, schema, provider }: TestContext) {
 
 			await db.insert(schema.defaults).values(data)
 
-			const result = await db.query.defaults.findFirst({
-				where: (defaults, { eq }) => eq(defaults.id, data.id),
-			})
+			const result = await db.query.defaults
+				.findFirst({
+					where: (defaults, { eq }) => eq(defaults.id, data.id),
+				})
+				.then(throwIfnull)
 			// --
 
 			expect(result).toBeDefined()
 			expect(
-				isAfter(result!.createdAt, now),
-				`Invalid now(), expected: ${result!.createdAt.toISOString()} to be greater than ${now.toISOString()}`
+				isAfter(result.createdAt, now),
+				`Invalid now(), expected: ${result.createdAt.toISOString()} to be greater than ${now.toISOString()}`
 			).toBe(true)
-			expect(result!.date.toISOString(), 'Invalid date').toBe(
+			expect(result.date.toISOString(), 'Invalid date').toBe(
 				'2024-01-23T00:00:00.000Z'
 			)
-			expect(result!.int, 'Invalid int').toBe(1)
-			expect(result!.boolean, 'Invalid boolean').toBe(true)
-			expect(result!.string, 'Invalid string').toBe('John')
+			expect(result.int, 'Invalid int').toBe(1)
+			expect(result.boolean, 'Invalid boolean').toBe(true)
+			expect(result.string, 'Invalid string').toBe('John')
 			if (provider === 'postgres') {
-				expect(result!.stringList, 'Invalid string list').toStrictEqual([
+				expect(result.stringList, 'Invalid string list').toStrictEqual([
 					'John',
 					'Doe',
 				])
 			}
-			expect(result!.bigint, 'Invalid bigint').toBe(1n)
-			expect(new Decimal(result!.decimal).toString(), 'Invalid decimal').toBe(
+			expect(result.bigint, 'Invalid bigint').toBe(1n)
+			expect(new Decimal(result.decimal).toString(), 'Invalid decimal').toBe(
 				'1.123'
 			)
-			expect(result!.float, 'Invalid float').toBe(1.123)
-			expect(result!.bytes.toString(), 'Invalid bytes').toBe('hello world')
+			expect(result.float, 'Invalid float').toBe(1.123)
+			expect(result.bytes.toString(), 'Invalid bytes').toBe('hello world')
 
 			if (provider !== 'sqlite') {
-				expect(result!.enum, 'Invalid enum').toBe('TypeTwo')
-				expect(result!.json, 'Invalid json').toStrictEqual({ foo: 'bar' })
+				expect(result.enum, 'Invalid enum').toBe('TypeTwo')
+				expect(result.json, 'Invalid json').toStrictEqual({ foo: 'bar' })
 			}
 
 			// --
@@ -59,16 +62,18 @@ export function testDefault({ db, schema, provider }: TestContext) {
 			await db.insert(schema.defaults).values(data)
 			// --
 
-			const result = await db.query.defaults.findFirst({
-				where: (defaults, { eq }) => eq(defaults.id, data.id),
-				columns: { alsoId: true, salt: true },
-			})
+			const result = await db.query.defaults
+				.findFirst({
+					where: (defaults, { eq }) => eq(defaults.id, data.id),
+					columns: { alsoId: true, salt: true },
+				})
+				.then(throwIfnull)
 
 			expect(result).toBeDefined()
-			expect(isCuid(result!.alsoId), 'Invalid when without custom code').toBe(
+			expect(isCuid(result.alsoId), 'Invalid when without custom code').toBe(
 				true
 			)
-			expect(result!.salt.length === 32, 'Invalid when with custom code').toBe(
+			expect(result.salt.length === 32, 'Invalid when with custom code').toBe(
 				true
 			)
 
@@ -84,18 +89,20 @@ export function testDefault({ db, schema, provider }: TestContext) {
 			})
 			// --
 
-			const result1 = await db.query.autoIncrements.findFirst({
-				where: (autoIncrements, { eq }) => eq(autoIncrements.ref, refs[0]),
-				columns: { id: true },
-			})
-			expect(result1).toBeDefined()
+			const result1 = await db.query.autoIncrements
+				.findFirst({
+					where: (autoIncrements, { eq }) => eq(autoIncrements.ref, refs[0]),
+					columns: { id: true },
+				})
+				.then(throwIfnull)
 
-			const result2 = await db.query.autoIncrements.findFirst({
-				where: (autoIncrements, { eq }) => eq(autoIncrements.ref, refs[1]),
-				columns: { id: true },
-			})
-			expect(result2).toBeDefined()
-			expect(result2!.id).toBe(result1!.id + 1)
+			const result2 = await db.query.autoIncrements
+				.findFirst({
+					where: (autoIncrements, { eq }) => eq(autoIncrements.ref, refs[1]),
+					columns: { id: true },
+				})
+				.then(throwIfnull)
+			expect(result2.id).toBe(result1.id + 1)
 
 			// --
 			await db
