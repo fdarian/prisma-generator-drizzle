80cd7fe00515a95c91ba7e8382c71942c531371a
farreldarian <farreldarian@users.noreply.github.com>
2024-04-09 20:48:19 +0700

    add single-file output config

--- packages/generator/src/shared/generator-context/index.ts
+++ packages/generator/src/shared/generator-context/index.ts
@@ -2,9 +2,18 @@ import type { GeneratorOptions } from '@prisma/generator-helper'
 import { type Config, parseConfig } from '~/lib/config'
 import { resolveModuleResolution } from './module-resolution'
 
+type Output = {
+	isSingleFile: boolean
+	path: string
+}
+
 type Generator = {
+	/**
+	 * @deprecated use `output.basePath` instead
+	 */
 	outputBasePath: string
 	moduleResolution?: string
+	output: Output
 	//
 	dmmf: GeneratorOptions['dmmf']
 	config: Config
@@ -16,10 +25,12 @@ let generator_: Generator | undefined
 
 export function initializeGenerator(options: GeneratorOptions) {
 	const config = parseConfig(options.generator.config)
+	const output = getOutputConfig(options)
 
 	const context: Generator = {
 		moduleResolution: config.moduleResolution ?? resolveModuleResolution(),
-		outputBasePath: getBasePath(options),
+		outputBasePath: output.path,
+		output,
 		//
 		dmmf: options.dmmf,
 		config,
@@ -29,10 +40,27 @@ export function initializeGenerator(options: GeneratorOptions) {
 	return context
 }
 
-function getBasePath(options: GeneratorOptions) {
+function getOutputConfig(options: GeneratorOptions): Output {
 	const basePath = options.generator.output?.value
 	if (!basePath) throw new Error('No output path specified')
-	return basePath
+
+	const isSingleFile = basePath.endsWith('.ts')
+	if (isSingleFile) {
+		return {
+			isSingleFile: true,
+			path: basePath,
+		}
+	}
+
+	return {
+		isSingleFile: false,
+		path: basePath,
+	}
+}
+
+function getParent(basePath: string) {
+	const paths = basePath.split('/')
+	return paths.length === 1 ? ['./'] : paths.slice(0, -1)
 }
 
 // #endregion
