545e8d3fda84438f53f22789c3192a015d328724
farreldarian <farreldarian@users.noreply.github.com>
2024-04-10 23:11:58 +0700

    test: field-level config

--- packages/usage/tests/configure-date-mode.test.ts
+++ packages/usage/tests/configure-date-mode.test.ts
@@ -18,7 +18,7 @@ test('global config', async () => {
 		}
 
 		generator drizzle { 
-			provider = "bunx prisma-generator-drizzle"
+			provider = "prisma-generator-drizzle"
 			dateMode = "string"
 			output = "drizzle.ts" 
 		} 
@@ -33,6 +33,39 @@ test('global config', async () => {
 	const output = await Bun.file(`${temp.basePath}/drizzle.ts`).text()
 	expect(output).toContain(
 		"date: timestamp('date', { mode: 'string', precision: 3 })"
+	)
+})
+
+test.only('field-level config', async () => {
+	const temp = await tempHandler.prepare()
+
+	await Bun.write(
+		getSchemaPath(temp),
+		`datasource db {
+			provider = "postgresql"
+			url      = env("PG_DATABASE_URL")
+		}
+
+		generator drizzle { 
+			provider = "prisma-generator-drizzle"
+			output = "drizzle.ts" 
+		} 
+
+		model User { 
+			id Int @id
+			normalDate DateTime 
+			/// drizzle.dateMode string
+			stringDate DateTime 
+		}`
+	)
+	await $`bun prisma generate --schema ${getSchemaPath(temp)}`.quiet()
+
+	const output = await Bun.file(`${temp.basePath}/drizzle.ts`).text()
+	expect(output).toContain(
+		"normalDate: timestamp('normalDate', { mode: 'date', precision: 3 })"
+	)
+	expect(output).toContain(
+		"stringDate: timestamp('stringDate', { mode: 'string', precision: 3 })"
 	)
 })
 
