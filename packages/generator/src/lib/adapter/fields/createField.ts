06a48cc645bee3987cbd2978665a9082ad25d95c
farreldarian <farreldarian@users.noreply.github.com>
2024-06-13 17:51:37 +0700

    feat: allow passing field opts

--- packages/generator/src/lib/adapter/fields/createField.ts
+++ packages/generator/src/lib/adapter/fields/createField.ts
@@ -16,7 +16,7 @@ export type DefineImport = {
 export interface CreateFieldInput {
 	field: DMMF.Field
 	imports?: ImportValue[]
-	func: string
+	func: string | (<T>(opts?: T) => string)
 	onDefault?: (
 		field: FieldWithDefault
 	) => { code: string; imports?: ImportValue[] } | undefined
@@ -30,8 +30,6 @@ export function createField(input: CreateFieldInput) {
 
 	let imports = input.imports ?? []
 
-	let func = `${input.func}`
-
 	const custom = getCustomDirective(field)
 	if (custom?.imports) {
 		imports = imports.concat(
@@ -42,6 +40,8 @@ export function createField(input: CreateFieldInput) {
 			)
 		)
 	}
+
+	let func = `${typeof input.func === 'string' ? input.func : input.func(custom?.field)}`
 
 	// .type<...>()
 	if (custom?.$type) {
