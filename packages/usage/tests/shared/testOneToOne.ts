f10750598d33374de58130b9b3df818d4e3a540e
farreldarian <farreldarian@users.noreply.github.com>
2024-06-25 00:08:11 +0700

    test: update

--- packages/usage/tests/shared/testOneToOne.ts
+++ packages/usage/tests/shared/testOneToOne.ts
@@ -6,15 +6,18 @@ import type { TestContext } from 'tests/utils/types'
 export function testOneToOne({ db, schema }: TestContext) {
 	const b: typeof schema.oneToOneBs.$inferInsert = { id: createId() }
 	const a: typeof schema.oneToOneAs.$inferInsert = { id: createId(), bId: b.id }
+	const c: typeof schema.oneToOneCs.$inferInsert = { id: createId(), bId: b.id }
 
 	describe('one to one', () => {
 		beforeAll(async () => {
 			await db.insert(schema.oneToOneBs).values(b)
+			await db.insert(schema.oneToOneCs).values(c)
 			await db.insert(schema.oneToOneAs).values(a)
 		})
 
 		afterAll(async () => {
 			await db.delete(schema.oneToOneAs).where(eq(schema.oneToOneAs.id, a.id))
+			await db.delete(schema.oneToOneCs).where(eq(schema.oneToOneCs.id, c.id))
 			await db.delete(schema.oneToOneBs).where(eq(schema.oneToOneBs.id, b.id))
 		})
 
@@ -50,20 +53,23 @@ export function testOneToOne({ db, schema }: TestContext) {
 			})
 		})
 
-		test('c.a (holds foreign key, required)', async () => {
-			const result = await db.query.oneToOneCs
+		test('optionality check', async () => {
+			const aResult = await db.query.oneToOneAs
 				.findFirst({
-					where: (oneToOneCs, { eq }) => eq(oneToOneCs.id, a.id),
-					with: {
-						b: true,
-					},
+					where: (oneToOneAs, { eq }) => eq(oneToOneAs.id, a.id),
+					with: { b: true },
 				})
 				.then(throwIfnull)
 
-			// expect(result).toStrictEqual({
-			// 	...b,
-			// 	a,
-			// })
+			const cResult = await db.query.oneToOneCs
+				.findFirst({
+					where: (oneToOneCs, { eq }) => eq(oneToOneCs.id, c.id),
+					with: { b: true },
+				})
+				.then(throwIfnull)
+
+			expectTypeOf(aResult.b).toBeNullable()
+			expectTypeOf(cResult.b).not.toBeNullable()
 		})
 	})
 }
