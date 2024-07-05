22d49c97bfbe9d6e9daaa78d6c15dc83b3080124
Farrel Darian <62016900+fdarian@users.noreply.github.com>
2024-07-05 18:53:45 +0700

    test: update bun to vitest

--- packages/usage/tests/shared/testFieldCustomization.ts
+++ packages/usage/tests/shared/testFieldCustomization.ts
@@ -1,6 +1,6 @@
-import { beforeAll, describe, expect, test } from 'bun:test'
-import fs from 'node:fs'
+import fs, { readFileSync } from 'node:fs'
 import type { TestContext } from 'tests/utils/types'
+import { beforeAll, describe, expect, test } from 'vitest'
 
 export type SomeBigInt = bigint
 
@@ -10,18 +10,18 @@ export function testFieldCustomization({ db, schema, provider }: TestContext) {
 	let content: string
 	beforeAll(async () => {
 		expect(fs.existsSync(OUTPUT_FILE)).toBe(true)
-		content = await Bun.file(OUTPUT_FILE).text()
+		content = readFileSync(OUTPUT_FILE, 'utf-8')
 	})
 
 	describe('allFields', () => {
 		test('should contain import', () => {
-			expect(content).toInclude(
+			expect(content).include(
 				"import type { SomeBigInt } from '~/tests/shared/testFieldCustomization'"
 			)
 		})
 
 		test('should contain correct field definition', () => {
-			expect(content).toInclude(
+			expect(content).include(
 				"allFields: bigint('allFields', { mode: 'bigint' }).$type<SomeBigInt>().$defaultFn(() => 1n)"
 			)
 		})
